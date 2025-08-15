fe4c05d8ec70b795ae3ae2d9fbbdb6697a6af693
Nurul Academy <nurulacademy@gmail.com>
2025-08-15 12:43:58 +0600

    set property added

---
+++ bin/set.dart
@@ -0,0 +1,16 @@
+void main() {
+  Set<String> colors = {"red", "green"};
+
+  colors.add("orange");
+  print(colors);
+  print(colors.elementAt(1));
+
+  colors.addAll({"black", "white"});
+  // colors.clear();
+  print(colors);
+  print(colors.length);
+  print(colors.isEmpty);
+  print(colors.isNotEmpty);
+  print(colors.contains("orange"));
+  // print(colors);
+}
