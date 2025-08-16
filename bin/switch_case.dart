5e35e9b1a29ec5b2676dbfe10b40db2ee262f0af
Nurul Academy <nurulacademy@gmail.com>
2025-08-16 15:04:27 +0600

    switch case added

---
+++ bin/switch_case.dart
@@ -0,0 +1,20 @@
+void main() {
+  String grade = "A+";
+
+  switch (grade) {
+    case "A+":
+      print("Exelent");
+      break;
+
+    case "A":
+      print("Good");
+      break;
+
+    case "A-":
+      print("Average");
+      break;
+
+    default:
+      print("Invalid Input");
+  }
+}
